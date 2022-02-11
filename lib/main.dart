9287f1e32244e7e326e537e96e836132e7ad4869
ChoJinmok <jjmok1127@gmail.com>
2022-02-11 12:47:18 +0900

    Flutter에서 UI 구성-버튼

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,13 @@
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
 
 void main() {
   runApp(App());
 }
 
 class App extends StatelessWidget {
+  final String name = 'Lizzy';
+  final double balance = 5194482;
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -13,6 +16,7 @@ class App extends StatelessWidget {
         body: Padding(
           padding: EdgeInsets.symmetric(horizontal: 20),
           child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
             children: [
               SizedBox(
                 height: 80,
@@ -35,10 +39,53 @@ class App extends StatelessWidget {
                         'Welcome back',
                         style: TextStyle(
                           color: Colors.white.withAlpha((255 * 0.8).round()),
-                          fontSize: 16,
+                          fontSize: 18,
                         ),
                       ),
                     ],
+                  ),
+                ],
+              ),
+              SizedBox(
+                height: 120,
+              ),
+              Text(
+                'Total Balance',
+                style: TextStyle(
+                    color: Colors.white.withAlpha((255 * 0.8).round()),
+                    fontSize: 22),
+              ),
+              Text(
+                '\$${NumberFormat('#,###').format(balance).replaceAll(',', ' ')}',
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 48,
+                  fontWeight: FontWeight.w800,
+                ),
+              ),
+              SizedBox(
+                height: 20,
+              ),
+              Row(
+                children: [
+                  // Container는 html의 div와 같은 (단순한 Box)역할을 한다.
+                  Container(
+                    decoration: BoxDecoration(
+                      color: Color(0xFFF2B33A),
+                      borderRadius: BorderRadius.circular(45),
+                    ),
+                    child: Padding(
+                      padding: EdgeInsets.symmetric(
+                        vertical: 20,
+                        horizontal: 50,
+                      ),
+                      child: Text(
+                        'Transfer',
+                        style: TextStyle(
+                          fontSize: 20,
+                        ),
+                      ),
+                    ),
                   ),
                 ],
               ),
